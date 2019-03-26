28996f94782b473447abe9e8444dc0b4cc4612f9
Jacob Logemann <820715+jakelogemann@users.noreply.github.com>
2019-03-26 12:34:27 -0400

    Import impure.nix when on NixOS.

--- nixos.nix
+++ nixos.nix
@@ -1,9 +1,9 @@
 # Used to install this framework's pkgs, lib & custom NixOS modules.
 # This is only useful in NixOS, Nix package users should instead use
 # `default.nix` which is ONLY the overlay.
-{ config, pkgs, lib, options, ... }: 
+{ config, pkgs, lib, options, ... }:
 
-{ 
-  nixpkgs.overlays = lib.mkAfter [(import ./default.nix)]; 
-  nix.nixPath = lib.mkAfter ["fnctlFunc=${./.}"];
+{
+  nixpkgs.overlays = lib.mkAfter [(import ./default.nix)];
+  nix.nixPath = lib.mkAfter ["fnctlFunc=${./impure.nix}"];
 }
