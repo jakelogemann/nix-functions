0be40909764b06a24c1bbf33196785bca6818d9e
jakelogemann <820715+jakelogemann@users.noreply.github.com>
2019-04-29 22:07:57 -0400

    fix nixos shim
    Signed-off-by: Jake Logemann <jake@lgmn.io>

--- nixos.nix
+++ nixos.nix
@@ -1,9 +1,8 @@
 # Used to install this framework's pkgs, lib & custom NixOS modules.
 # This is only useful in NixOS, Nix package users should instead use
 # `default.nix` which is ONLY the overlay.
-{ config, pkgs, lib, options, ... }:
-
-{
-  nixpkgs.overlays = lib.mkAfter [(import ./default.nix)];
-  nix.nixPath = lib.mkAfter ["fnctlFunc=${./impure.nix}"];
-}
+{ config, pkgs, lib, options, ... }: with lib;
+{ config = {
+  nixpkgs.overlays = [(import ./default.nix)];
+  nix.nixPath = ["fnctlFunc=${./impure.nix}"];
+}; }
